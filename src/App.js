f88aa3ddab15fbdcb5f44dac04202c4145936b37
Vidya <vidyasba@gmail.com>
2024-02-17 12:24:17 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //States for input capturing value 
+  const [email,setEmail]= useState('');
+  const [password,setPassword]=useState('');
+  const [cpassword,setCpassword]=useState('');
+
+  //States for handling the input validations
+  const [emailValid,setEmailValid]=useState(false);
+  const [passwordValid,setPasswordValid]=useState(false);
+  const [CpassowrdValid,setCpasswordValid]=useState(false);
+
+  const handleEmailChange =(e) =>{
+    const value=e.target.value;
+    setEmail(value);
+    setEmailValid(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value));
+    }
+
+  const handlePasswordChange=(e) =>{
+    const value =e.target.value;
+    setPassword(value);
+    setPasswordValid(value.length>=8)
+  }
+  const handleConfirmPassword=(e)=>{
+    const value=e.target.value;
+    setCpassword(value);
+    setCpasswordValid(value===password);
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(emailValid && passwordValid && CpassowrdValid){
+      alert('Form submitted successfully');
+    }else{
+      alert('cant submit the form');
+    }
+  }
+  const clearForm=() =>{
+  setEmail('');
+  setPassword('');
+  setCpassword('');
+  setEmailValid(false);
+  setPasswordValid(false);
+  setCpasswordValid(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-Container">
+      <form class="form" onSubmit={handleSubmit}>
+
+        <label htmlFor="email">Email:</label><br />
+        <input 
+        type="email" value={email} onChange={handleEmailChange}
+        style={{borderColor:emailValid ?'green' : 'red'}} />
+        {!emailValid && <p id="emailp">Please enter a valid email</p>}
+        <br/>
+
+        <label htmlFor="password">Password:</label><br />
+        <input type="password" value={password} onChange={handlePasswordChange}
+        style={{borderColor:passwordValid? 'green' :'red'}}
+        ></input>
+        {!passwordValid && <p id="emailp" >Password must be at least 8 characters long</p>}
+        <br/>
+
+        <label htmlFor="cpass">Confirm Password:</label><br />
+        <input type="password"
+        value={cpassword}
+        onChange={handleConfirmPassword}
+        style={{borderColor:CpassowrdValid ?'green':'red'}}></input>
+        {!CpassowrdValid && <p  id="emailp" >Passwords do not match</p>}
+
+        <br/><br/>
+        <button id="signup" onClick={clearForm}>Sign Up</button>
+      </form>
     </div>
   );
 }
